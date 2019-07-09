69541f2363c54e43b437f85dc5c56824ecfc2920
Shao <shao.p.tessier@gmail.com>
2019-07-09 08:47:38 -0400

    Adding second scene choice

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour{
+    public GameObject flashText;
+
+    // Start is called before the first frame update
+    void Start() {
+        //Flash text
+        InvokeRepeating("flashTheText", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //If mouse click load the scene choice
+        if (Input.GetMouseButtonUp(0)) {
+            SceneManager.LoadScene("Choice");
+        }
+    }
+
+    // Flash the text
+    void flashTheText() {
+        if (flashText.activeInHierarchy) {
+            flashText.SetActive(false);
+        }
+        else {
+            flashText.SetActive(true);
+        }
+    }
+}
